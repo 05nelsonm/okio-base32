8b432a641e54ff7bf2aba5616e5369e4ed0dac52
Matthew Nelson <developer@matthewnelson.io>
2021-02-11 13:31:39 -0500

    initialize base32 implementation module

---
+++ base32/src/test/java/io/matthewnelson/base32/Base32UnitTest.kt
@@ -0,0 +1,73 @@
+package io.matthewnelson.base32
+
+import com.google.common.io.BaseEncoding
+import okio.internal.commonToUtf8String
+import org.junit.Assert
+import org.junit.Test
+import java.io.CharArrayWriter
+import java.security.SecureRandom
+
+/**
+ * Tests decode/encode output against Guava
+ * */
+class Base32UnitTest {
+
+    private val sRandom: SecureRandom = SecureRandom()
+    private val testDepth: Int = 1_000
+
+    @Test
+    fun `Okio base32 _decode_ matches Guava output`() {
+        val base32Chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567".toCharArray()
+
+        for (i in 0 until testDepth) {
+
+            val charArrayWriter = CharArrayWriter(i)
+
+            // Build character array of randomly chosen base32 characters that varies in size
+            repeat(i) {
+                charArrayWriter.append(base32Chars[sRandom.nextInt(base32Chars.size)])
+            }
+
+            charArrayWriter.toString().let { randomEncoding ->
+
+                randomEncoding.decodeBase32ToArray()?.let { okioDecoded ->
+
+                    val guavaDecoded = BaseEncoding.base32().decode(randomEncoding)
+                    Assert.assertEquals(okioDecoded.size, guavaDecoded.size)
+                    Assert.assertEquals(okioDecoded.commonToUtf8String(), guavaDecoded.commonToUtf8String())
+
+                } ?: let {
+                    // Okio base32 returned null, so Guava BaseEncoding should
+                    // throw an exception for the given string value 'randomEncoding'
+                    try {
+                        BaseEncoding.base32().decode(randomEncoding)
+
+                        // Fail test if IllegalArgumentException is not thrown
+                        Assert.fail()
+                    } catch (e: IllegalArgumentException) {
+                        // Expected exception. Pass
+                    }
+                }
+
+            }
+        }
+    }
+
+    @Test
+    fun `Okio base32 _encode_ matches Guava output`() {
+        for (i in 0 until testDepth) {
+            val randomByteArray = ByteArray(i)
+
+            // Build ByteArray of randomly chosen bytes
+            for (j in 0 until i) {
+                randomByteArray[j] = sRandom.nextInt().toByte()
+            }
+
+            randomByteArray.encodeBase32().let { okioEncoded ->
+                BaseEncoding.base32().encode(randomByteArray).let { guavaEncoded ->
+                    Assert.assertEquals(okioEncoded, guavaEncoded)
+                }
+            }
+        }
+    }
+}
